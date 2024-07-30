da0fefc1b003dbc85a70fa91081b07d35ad3910b
Wellington Santos <wellingtonasc38@gmail.com>
2024-07-29 23:27:11 -0300

    feat: Remove unused SVG file and update section containers
    The commit removes an unused SVG file and updates the `SectionContainer` component to accept a `minHeight` prop. This change improves the layout of the `PartnersSection` and `SupportersSection` components.

--- src/components/SectionContainer/SectionContainer.jsx
+++ src/components/SectionContainer/SectionContainer.jsx
@@ -3,10 +3,10 @@ import { Container, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 
 
-const SectionContainer = ({ title, description = null, children }) => {
+const SectionContainer = ({ title, description = null, minHeight='60vh', children }) => {
 
     return (
-        <Container maxWidth="xl" sx={{ padding: '2rem 0', textAlign: 'center', minHeight: '60vh' }}>
+        <Container maxWidth="xl" sx={{ padding: '2rem 0', textAlign: 'center', minHeight: minHeight  }}>
             <Typography variant="h4" component="h2" gutterBottom>
                 {title}
             </Typography>
@@ -27,6 +27,7 @@ SectionContainer.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string,
     children: PropTypes.node.isRequired,
+    minHeight: PropTypes.string,
 };
 
 export default SectionContainer;
