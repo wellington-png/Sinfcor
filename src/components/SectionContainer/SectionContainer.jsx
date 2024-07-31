0758d851a7b46c645c3bc9ee95d75d915a23e581
Wellington Santos <wellingtonasc38@gmail.com>
2024-07-31 00:34:50 -0300

    divider

--- src/components/SectionContainer/SectionContainer.jsx
+++ src/components/SectionContainer/SectionContainer.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 
-const SectionContainer = ({ title, description = null, minHeight='60vh', children }) => {
+
+const Divider = styled.div`
+    font-size: 30px;
+    display: flex;
+    align-items: center;
+    &::before, &::after {
+        content: '';
+        height: 1px;
+        background-color: silver;
+        flex-grow: 1;
+        margin: 0 20px;
+    }
+`;
+
+
+const SectionContainer = ({ title, description = null, minHeight = '60vh', children }) => {
 
     return (
-        <Container maxWidth="xl" sx={{ padding: '2rem 0', textAlign: 'center', minHeight: minHeight  }}>
-            <Typography variant="h4" component="h2" gutterBottom>
-                {title}
-            </Typography>
+        <Container maxWidth="xl" sx={{ padding: '2rem 0', textAlign: 'center', minHeight: minHeight }}>
+            {/* Text Divider */}
+            <Divider variant="h4" gutterBottom>
+                <Typography variant="h4" component="h2">
+                    {title}
+                </Typography>
+            </Divider>
+
             {
                 description && (
                     <Typography variant="body1" gutterBottom>
