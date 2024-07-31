b58449d2392606334efc223c9564045d6830ed56
Wellington Santos <wellingtonasc38@gmail.com>
2024-07-31 00:51:32 -0300

    teste font

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -33,42 +33,45 @@ const Header = () => {
             <Container maxWidth="xl" sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
                 <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
                     <Container maxWidth="sm" sx={{ margin: 0, display: 'flex', flexDirection: "column", rowGap: '2rem' }}>
-                        <Title variant="h3" component="h1" gutterBottom>
+                        <Title variant="h3" component="h1" gutterBottom sx={{
+                            fontFamily: 'Poppins',
+                            fontWeight: 'bold',
+                        }}>
                             Simpósio de Informática de Corrente
                         </Title>
-                        <Typography variant="h5" component="h2" gutterBottom sx={{
-                            display: {
-                                xs: 'none',
-                                md: 'block'
+                    <Typography variant="h5" component="h2" gutterBottom sx={{
+                        display: {
+                            xs: 'none',
+                            md: 'block'
+                        }
+                    }}>
+                        O SINFCOR é um evento organizado pelo Instituto Federal do Piauí - Campus Corrente, que tem como objetivo principal promover a integração entre estudantes, professores e profissionais da área de informática.
+                    </Typography>
+                    <Box sx={{ display: 'flex', gap: '2rem' }}>
+                        <Button sx={{
+                            backgroundColor: '#F5167E',
+                            padding: '10px 20px',
+                            borderRadius: '20px',
+                            '&:hover': {
+                                backgroundColor: '#F5167Ec1',
                             }
-                        }}>
-                            O SINFCOR é um evento organizado pelo Instituto Federal do Piauí - Campus Corrente, que tem como objetivo principal promover a integração entre estudantes, professores e profissionais da área de informática.
-                        </Typography>
-                        <Box sx={{ display: 'flex', gap: '2rem' }}>
-                            <Button sx={{
-                                backgroundColor: '#F5167E',
+                        }} variant="contained" color="primary">Cadastro</Button>
+                        <Button
+                            sx={{
                                 padding: '10px 20px',
                                 borderRadius: '20px',
+                                borderColor: '#F5167E',
+                                color: '#F5167E',
                                 '&:hover': {
-                                    backgroundColor: '#F5167Ec1',
+                                    backgroundColor: '#F5167E1f',
                                 }
-                            }} variant="contained" color="primary">Cadastro</Button>
-                            <Button
-                                sx={{
-                                    padding: '10px 20px',
-                                    borderRadius: '20px',
-                                    borderColor: '#F5167E',
-                                    color: '#F5167E',
-                                    '&:hover': {
-                                        backgroundColor: '#F5167E1f',
-                                    }
-                                }}
-                                variant="outlined" color="primary">Saiba mais</Button>
-                        </Box>
-                    </Container>
-                </Box>
+                            }}
+                            variant="outlined" color="primary">Saiba mais</Button>
+                    </Box>
             </Container>
         </Box>
+            </Container >
+        </Box >
     );
 };
 
