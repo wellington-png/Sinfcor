36d795629bedd2276ba2410b325090a25571343b
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-08-06 20:53:22 -0300

    chore: Add download functionality for timeline PDF file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import Wave from '../../assets/wave.png';
 import styled from 'styled-components';
 import ParticlesComponent from '../ParticlesComponent/ParticlesComponent';
-
+import Timeline from '../../assets/timeline/cronogramaoficial.pdf';
 const Title = styled(Typography)`
     background: linear-gradient(90deg, #5324c6 0%, #840fbe 100%);
     -webkit-background-clip: text;
@@ -16,6 +16,11 @@ const Title = styled(Typography)`
 `;
 
 const Header = () => {
+
+    const onDownload = () => {
+        window.open(Timeline);
+    }
+
     return (
         <Box
             component="header"
@@ -37,37 +42,43 @@ const Header = () => {
                         <Title variant="h3" component="h1" gutterBottom>
                             Simpósio de Informática de Corrente
                         </Title>
-                    <Typography variant="h5" component="h2" gutterBottom sx={{
-                        display: {
-                            xs: 'none',
-                            md: 'block'
-                        }
-                    }}>
-                        O SINFCOR é um evento organizado pelo Instituto Federal do Piauí - Campus Corrente, que tem como objetivo principal promover a integração entre estudantes, professores e profissionais da área de informática.
-                    </Typography>
-                    <Box sx={{ display: 'flex', gap: '2rem' }}>
-                        <Button sx={{
-                            backgroundColor: '#F5167E',
-                            padding: '10px 20px',
-                            borderRadius: '20px',
-                            '&:hover': {
-                                backgroundColor: '#F5167Ec1',
+                        <Typography variant="h5" component="h2" gutterBottom sx={{
+                            display: {
+                                xs: 'none',
+                                md: 'block'
                             }
-                        }} variant="contained" color="primary">Cadastro</Button>
-                        <Button
-                            sx={{
-                                padding: '10px 20px',
-                                borderRadius: '20px',
-                                borderColor: '#F5167E',
-                                color: '#F5167E',
-                                '&:hover': {
-                                    backgroundColor: '#F5167E1f',
-                                }
-                            }}
-                            variant="outlined" color="primary">Saiba mais</Button>
-                    </Box>
-            </Container>
-        </Box>
+                        }}>
+                            O SINFCOR é um evento organizado pelo Instituto Federal do Piauí - Campus Corrente, que tem como objetivo principal promover a integração entre estudantes, professores e profissionais da área de informática.
+                        </Typography>
+                        <Box
+                            sx={{ display: 'flex', gap: '2rem' }}>
+                            <Button 
+                            href='https://suap.ifpi.edu.br/eventos/inscricao/1522/'
+                                sx={{
+                                    backgroundColor: '#F5167E',
+                                    padding: '10px 20px',
+                                    borderRadius: '20px',
+                                    '&:hover': {
+                                        backgroundColor: '#F5167Ec1',
+                                    }
+                                }} variant="contained" color="primary">Cadastro</Button>
+                            <Button
+                                onClick={onDownload}
+                                sx={{
+                                    padding: '10px 20px',
+                                    borderRadius: '20px',
+                                    borderColor: '#F5167E',
+                                    color: '#F5167E',
+                                    '&:hover': {
+                                        backgroundColor: '#F5167E1f',
+                                    }
+                                }}
+
+                                variant="outlined" color="primary"> Cronograma</Button>
+
+                        </Box>
+                    </Container>
+                </Box>
             </Container >
         </Box >
     );
