419c8859681cda686eaafbc46be8ea62c99e93c9
LivramentoAmaral <marcosdoamaral2002@gmail.com>
2024-08-06 21:10:53 -0300

    feat: Add carousel image styles and update dependencies

--- src/components/ActivitySession/data.js
+++ src/components/ActivitySession/data.js
@@ -3,44 +3,105 @@ import palestraImage from '../../assets/rear-view-programmer-working-all-night-l
 const activities = [
     {
         id: 1,
-        title: 'Corrente Informatics Symposium',
-        description: 'Detailed description of the symposium, including objectives, target audience, and speakers.',
+        title: 'Introdução à HTML e CSS ',
+        description: '',
         image: palestraImage,
         date: '15/08/2024',
         location: 'IFPI - Corrente Campus Auditorium',
-        speakers: ['Prof. Dr. João Silva', 'Profa. Dra. Maria Souza'],
+        speakers: ['Ana Francisa', 'Clara Cunha','Ester Lago','Leticia Gonçalves'],
         registration: 'https://registration-link'
     },
     {
         id: 2,
-        title: 'Tech Conference 2024',
-        description: 'An annual tech conference focusing on emerging technologies.',
+        title: 'Introdução a programação ',
+        description: '',
         image: palestraImage,
         date: '15/08/2024',
         location: 'IFPI - Corrente Campus Auditorium',
-        speakers: ['Prof. Dr. João Silva', 'Profa. Dra. Maria Souza'],
+        speakers: ['Eduardo dos Santos Lustosa', 'Henrique Castro do Nascimento','Tiago de Souza Mouta','Thaemilly Souza Seriqueira'],
         registration: 'https://registration-link'
     },
     {
         id: 3,
-        title: 'AI and Future Trends',
-        description: 'Exploring the future of AI and its impact on various industries.',
+        title: 'Introdução a programação com Python: Aprendizagem simplificada através de jogos',
+        description: '',
         image: palestraImage,
         date: '15/08/2024',
         location: 'IFPI - Corrente Campus Auditorium',
-        speakers: ['Prof. Dr. João Silva', 'Profa. Dra. Maria Souza'],
+        speakers: ['Guilherme Santo Costa', 'Pedro Henrique Vogado Maia','Luiara Vieira'],
         registration: 'https://registration-link'
     },
     {
         id: 4,
-        title: 'Innovative Technologies Expo',
-        description: 'A showcase of the latest innovations in technology.',
+        title: 'Operação de drones',
+        description: '',
         image: palestraImage,
         date: '15/08/2024',
         location: 'IFPI - Corrente Campus Auditorium',
-        speakers: ['Prof. Dr. João Silva', 'Profa. Dra. Maria Souza'],
+        speakers: ['Prof. Felipe Gonçalves'],
+        registration: 'https://registration-link'
+    },
+    {
+        id: 5,
+        title: 'Latex para criação de artigos científicos',
+        description: '',
+        image: palestraImage,
+        date: '15/08/2024',
+        location: 'IFPI - Corrente Campus Auditorium',
+        speakers: ['Claudemiro Barbosa Nogueira Bispo','Ana Vitoria Dias Carvalho','Heitor Garcez Martins Louzeiro','Felipe Lustosa Carvalho'],
+        registration: 'https://registration-link'
+    },
+    {
+        id: 6,
+        title: 'Power BI: Visualização e Análise de Dados',
+        description: '',
+        image: palestraImage,
+        date: '15/08/2024',
+        location: 'IFPI - Corrente Campus Auditorium',
+        speakers: ['Daniel de Oliveira Cunha','Carlos Omar Barros Jagnow','Jhenik Teixeira de Brito','Gustavo Moreira Cruz'],
+        registration: 'https://registration-link'
+    },
+    {
+        id: 7,
+        title: 'Introdução ao desenvolvimento Front-End com Angular',
+        description: '',
+        image: palestraImage,
+        date: '15/08/2024',
+        location: 'IFPI - Corrente Campus Auditorium',
+        speakers: ['Jhonatas Gomes Ribeiro'],
+        registration: 'https://registration-link'
+    },
+    {
+        id: 8,
+        title: 'Aplicativos de escritório',
+        description: '',
+        image: palestraImage,
+        date: '15/08/2024',
+        location: 'IFPI - Corrente Campus Auditorium',
+        speakers: ['Ana Raquel Martins Carvalho','Jean Carlos Paiva Araújo','Livya Cunha da Luz','Maria Hellena Gomes Damasceno','Nádia Evellyn Pinheiro de Freitas Louzeiro'],
+        registration: 'https://registration-link'
+    },
+    {
+        id: 9,
+        title: 'Introdução ao Canva',
+        description: '',
+        image: palestraImage,
+        date: '15/08/2024',
+        location: 'IFPI - Corrente Campus Auditorium',
+        speakers: ['Prof. Israel'],
+        registration: 'https://registration-link'
+    },
+    {
+        id: 10,
+        title: 'Educação financeira',
+        description: '',
+        image: palestraImage,
+        date: '15/08/2024',
+        location: 'IFPI - Corrente Campus Auditorium',
+        speakers: ['Rafael Batista (licen.matemática)'],
         registration: 'https://registration-link'
     }
+
 ];
 
 export default activities;
