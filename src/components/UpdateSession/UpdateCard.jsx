a4ea4ef60303bf5b8fc5e494dccf5de2eaa117b1
Wellington Santos <wellingtonasc38@gmail.com>
2024-08-16 11:59:38 -0300

    atualizaçãoes post

--- src/components/UpdateSession/UpdateCard.jsx
+++ src/components/UpdateSession/UpdateCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Img from '../../assets/rear-view-programmer-working-all-night-long.jpg';
+import cartaz from '../../assets/SINFCOR.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './carouselStyles.css'; // Adicionei esta linha para importar o CSS
 
@@ -13,6 +14,7 @@ function IndividualIntervalsExample() {
           src={Img}
           alt="First slide"
           height={'600'}
+          style={{ objectFit: 'cover' }}
         />
         <Carousel.Caption>
         </Carousel.Caption>
@@ -20,29 +22,9 @@ function IndividualIntervalsExample() {
       <Carousel.Item interval={1500}>
         <img
           className="d-block w-100 carousel-img"
-          src={Img}
+          src={cartaz}
           alt="Second slide"
-          height={'600'}
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100 carousel-img"
-          src={Img}
-          alt="Third slide"
-          height={'600'}
-        />
-        <Carousel.Caption>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={1500}>
-        <img
-          className="d-block w-100 carousel-img"
-          src={Img}
-          alt="Fourth slide"
-          height={'600'}
+          style={{ objectFit: 'contain' }}
         />
         <Carousel.Caption>
         </Carousel.Caption>
